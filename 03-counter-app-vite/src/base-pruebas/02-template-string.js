e5681961b974db42230092571c8b05a67db0d5fd
Melissa <malvarez@datagas.com.co>
2022-06-30 17:21:32 -0500

    avances del curso en la parte de test

---
+++ 03-counter-app-vite/src/base-pruebas/02-template-string.js
@@ -0,0 +1,6 @@
+
+function getSaludo(nombre) {
+    return 'Hola ' + nombre;
+}
+
+console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
---
+++ 03-counter-app-vite/src/test/02-template-string.test.js
@@ -0,0 +1,12 @@
+
+function getSaludo(nombre) {
+    return 'Hola ' + nombre;
+}
+
+console.log( `Este es un texto: ${ getSaludo( nombre ) }  ` );
+
+describe('PRUEBAS EN 02-template-string', () => {
+    test('get saludo debe retornar hola melissa', () => { 
+
+    })
+})
