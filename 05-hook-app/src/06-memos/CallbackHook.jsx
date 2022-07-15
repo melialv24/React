06f4f427d61af7407fb534a9e46d771afb5db489
Melissa <malvarez@datagas.com.co>
2022-07-15 17:04:36 -0500

    finiquitado los hooks

--- 05-hook-app/src/06-memos/CallbackHook.jsx
+++ 05-hook-app/src/06-memos/CallbackHook.jsx
@@ -1,23 +1,34 @@
-import { useCallback, useState } from "react"
-import { ShowIncrement } from "./ShowIncrement"
+import { useCallback, useEffect, useState } from 'react';
+import { ShowIncrement } from './ShowIncrement';
+
 
 export const CallbackHook = () => {
 
-    const [counter, setCounter] = useState(10)
+    const [counter, setCounter] = useState( 10 );
 
-    const incrementF = useCallback(
-        () => {
-            setCounter( (counter) => counter + 1)
-        },
-        [],
-    )
+    const incrementFather = useCallback(
+      (value) => {
+        setCounter( (c) => c + value );
+      },
+      [],
+    );
+
+    useEffect(() => {
+      // incrementFather();
+    }, [incrementFather])
     
+    
+    // const incrementFather = () => {
+    //     setCounter( counter + 1);
+    // }
+
+
     return (
         <>
-            <h1> useCallback Hook: {counter} </h1>
-            <hr/>
+            <h1>useCallback Hook: { counter } </h1>
+            <hr />
 
-            <ShowIncrement increment={ incrementF } />
+            <ShowIncrement increment={ incrementFather } />
         </>
     )
 }
--- 05-hook-app/src/06-memos/ShowIncrement.jsx
+++ 05-hook-app/src/06-memos/ShowIncrement.jsx
@@ -1,13 +1,17 @@
+import React from "react";
 
-export const ShowIncrement = ({ increment }) => {
+export const ShowIncrement = React.memo( ({ increment }) => {
+
+    console.log(' Me volví a generar :( ');
+
     return (
         <button
             className="btn btn-primary"
             onClick={() => {
-                increment()
+                increment( 5 );
             }}
         >
             Incrementar
         </button>
     )
-}
+})
