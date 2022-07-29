4c5f2b1a5ab97b0619ec347a5cb5b65875d56dae
Melissa <malvarez@datagas.com.co>
2022-07-29 17:17:08 -0500

    terminada la primera seccion de la app de heroes

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,37 +1,44 @@
-import { HeroCard } from "../components/HeroCard"
-import { useForm } from "../../hooks/useForm"
-import queryString from 'query-string'
-import { useLocation, useNavigate } from "react-router-dom"
+import { HeroCard } from "../components/HeroCard";
+import { useForm } from "../../hooks/useForm";
+import queryString from "query-string";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getHeroesByName } from "../helpers";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [heroes, setHeroes] = useState([]);
 
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  const {q=''} = queryString.parse(location.search)
+  const { q = "" } = queryString.parse(location.search);
+  useEffect(() => {
+    const res = getHeroesByName(q);
+    setHeroes(res);
+  }, [q]);
 
   const { searchText, onInputChange } = useForm({
-    searchText: ''
-  })
+    searchText: q,
+  });
 
   const onSearchSubmit = (e) => {
-    e.preventDefault()
-    if(searchText.trim().length <= 1) return
+    e.preventDefault();
+    if (searchText.trim().length <= 1) return;
 
-    navigate(`?q=${searchText.toLowerCase().trim()}`)
-  }
+    navigate(`?q=${searchText.toLowerCase().trim()}`);
+  };
 
   return (
     <>
       <h1>Search</h1>
-      <hr/>
+      <hr />
       <div className="row">
         <div className="col-5">
           <h4>Searching</h4>
-          <hr/>
-          <form onSubmit={ onSearchSubmit }>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
             <input
-              type='text'
+              type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
@@ -40,31 +47,31 @@ export const SearchPage = () => {
               onChange={onInputChange}
             />
 
-            <button
-              className="btn btn-outline-primary mt-1"
-              type="submit"
-            >
+            <button className="btn btn-outline-primary mt-1" type="submit">
               search
             </button>
           </form>
         </div>
 
         <div className="col-7">
-            <h4>Results</h4>
-            <hr/>
+          <h4>Results</h4>
+          <hr />
 
-            <div className="alert alert-primary">
-                Search a hero
-            </div>
-            <div className="alert alert-danger">
-                There´s no result with {q}
-            </div>
+          {q === "" && heroes.length === 0 && (
+            <div className="alert alert-primary">Search a hero</div>
+          )}
 
-            {/*<HeroCard key={}/>*/}
+          {q.length > 0 && heroes.length === 0 && (
+            <div className="alert alert-danger">There´s no result with {q}</div>
+          )}
 
+          {/*<div className="alert alert-danger">There´s no result with {q}</div>*/}
+
+          {heroes.map((heroe) => (
+            <HeroCard key={heroe.id} {...heroe} />
+          ))}
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
